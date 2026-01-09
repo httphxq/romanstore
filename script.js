6de3b6e3a101268b33823240701595a8f228e2ad
Анастасия <httphxq@gmail.com>
2026-01-09 17:13:47 +0300

    script js: hyperlinks inside the page

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('[data-target]').forEach(link => {
+    link.addEventListener('click', () => {
+        const targetId = link.dataset.target;
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
